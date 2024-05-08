6be4b0ca823b61019cb542c1470346d8c10025a6
DerHausmeisterKrause <Marcel.asboeck2000@web.de>
2024-05-08 21:54:56 +0200

    EazeBloom_V1.1
    new code it works only temp && Hydro at the web

--- EazeBloom_V1/EazeBloom_V1.ino
+++ EazeBloom_V1/EazeBloom_V1.ino
@@ -1,87 +1,42 @@
 #include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
 #include <DHT.h>
 
 const char* ssid = "FRITZ!Box-Asboeck";
 const char* password = "Robby2222!";
 
-const int DHTPin = D4; // Pin, an dem der DHT11-Sensor angeschlossen ist
-const int DHTType = DHT11; // Typ des DHT-Sensors
+#define DHTPIN 2     // DHT11 Sensor Pin - GPIO2 (D4)
+#define DHTTYPE DHT11   // DHT 11
 
-DHT dht(DHTPin, DHTType);
+DHT dht(DHTPIN, DHTTYPE);
 
-WiFiServer server(80);
+ESP8266WebServer server(80);
+
+void handleRoot() {
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+
+  String webpage = "Humidity: " + String(humidity) + "%<br>Temperature: " + String(temperature) + "°C";
+  server.send(200, "text/html", webpage);
+}
 
 void setup() {
   Serial.begin(115200);
-  delay(10);
-
   dht.begin();
-
-  // Verbindung zum WiFi-Netzwerk herstellen
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
+  pinMode(DHTPIN, INPUT);
   WiFi.begin(ssid, password);
-
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
   }
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-
-  // Start des Web Servers
+  Serial.println("Connected to WiFi");
+  Serial.println(WiFi.localIP());
+  server.on("/", handleRoot);
   server.begin();
-  Serial.println("Server started");
-
-  Serial.print("Use this URL to connect: ");
-  Serial.print("http://");
-  Serial.print(WiFi.localIP());
-  Serial.println("/");
 }
 
 void loop() {
-  WiFiClient client = server.available();
-  if (!client) {
-    return;
-  }
-
-  Serial.println("new client");
-  client.println("HTTP/1.1 200 OK");
-  client.println("Content-Type: text/html");
-  client.println("Connection: close");
-  client.println();
-
-  client.println("<!DOCTYPE HTML>");
-  client.println("<html>");
-
-  client.println("<head>");
-  client.println("<meta http-equiv='refresh' content='5'>"); // Aktualisiere die Seite alle 5 Sekunden
-  client.println("<title>ESP8266 DHT11 Web Server</title>");
-  client.println("</head>");
-
-  client.println("<body>");
-
-  client.println("<h1>ESP8266 DHT11 Web Server</h1>");
-
-  client.println("<h2>Current Temperature and Humidity</h2>");
-  
-  // Sensorwerte abrufen
-  float temperature = dht.readTemperature();
-  float humidity = dht.readHumidity();
-
-  client.print("<p>Temperature: ");
-  client.print(temperature);
-  client.println(" °C</p>");
-  client.print("<p>Humidity: ");
-  client.print(humidity);
-  client.println(" %</p>");
-
-  client.println("</body>");
-  client.println("</html>");
-
-  delay(1000); // Eine Sekunde warten
+  server.handleClient();
+  delay(1000); // Jede Sekunde aktualisieren
 }
